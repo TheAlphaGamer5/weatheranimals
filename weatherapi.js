00884aacced2876e3e7457212a1d09c74afdfe14
Andrew Marin Heusser <heusser.andrew@gmail.com>
2017-02-09 11:19:50 -0800

    im using git hub again in case of crashes

---
+++ weatherapi.js
@@ -0,0 +1,20 @@
+var apiKey = "a3949c3b11f64b8ba9330408170902"
+var weatherURL = ""
+
+function showAnimal(data) {
+
+}
+
+function makeRequest() {
+  $.ajax({
+    url: // what should go here?
+    success: function(data) {
+      // what should go here?
+    }
+  })
+}
+
+
+function submit() {
+
+}

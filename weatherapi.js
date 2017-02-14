6c48f6690b21e606ba4d358d23c05d429c2b1f30
Andrew Marin Heusser <heusser.andrew@gmail.com>
2017-02-14 10:44:59 -0800

    eh doing stuff

--- weatherapi.js
+++ weatherapi.js
@@ -1,5 +1,6 @@
 var apiKey = "a3949c3b11f64b8ba9330408170902"
 var weatherURL = ""
+var input
 
 function showAnimal(data) {
 
@@ -7,14 +8,17 @@ function showAnimal(data) {
 
 function makeRequest() {
   $.ajax({
-    url: // what should go here?
+    url: weatherURL// what should go here?
     success: function(data) {
       // what should go here?
+      showAnimal(data)
     }
   })
 }
 
 
 function submit() {
+  input = document.getElementById('zipcode').value;
+weatherURL = "https://api.apixu.com/v1/current.json?key=" + apiKey + "&q=" + input
 
 }

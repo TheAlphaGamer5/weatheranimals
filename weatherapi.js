2d45779363213d1a35a264d68c1aa55ff8317572
Andrew Marin Heusser <heusser.andrew@gmail.com>
2017-02-27 12:01:39 -0800

    sdASdvgzdyjhckyghjxdzgr
    Done.....kinda

--- weatherapi.js
+++ weatherapi.js
@@ -4,6 +4,22 @@ var input
 
 function showAnimal(data) {
 var temp = data.current.temp_f
+if (temp > 90){
+ document.getElementById("imagecontainer").innerHTML = "<img src='images/Cloudy.jpg' width='500' height='500'>"
+}
+if (temp > 75){
+ document.getElementById("imagecontainer").innerHTML = "<img src='images/God.jpg' width='500' height='500'>"
+}
+if (temp > 60){
+ document.getElementById("imagecontainer").innerHTML = "<img src='images/Winter_is_Coming.jpg' width='500' height='500'>"
+}
+if (temp > 40){
+ document.getElementById("imagecontainer").innerHTML = "<img src='images/midtemp.jpg' width='500' height='500'>"
+}
+if (temp >20){
+ document.getElementById("imagecontainer").innerHTML = "<img src='images/Hace_Calor.jpg' width='500' height='500'>"
+}
+document.getElementById("imagecontainer").innerHTML = temp + "degrees fahrenheit"
 }
 
 function makeRequest() {
